43569c24b37ebde4cd392b68016007d4969e479b
Jforbus <109490597+Jforbus@users.noreply.github.com>
2022-11-17 20:22:28 -0600

    Technical Analysis of Suspension Coil PSI

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,3 +1,5 @@
+## Linear regression for MPG ##
+
 # import package
 library(dplyr)
 
@@ -9,3 +11,19 @@ model <- lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clear
 
 # gather summary statistics
 summary(model)
+
+
+## Technical Analysis of Suspension Coils ##
+
+# load data from csv
+sus_coil <- read.csv("Resources/Suspension_Coil.csv",check.names = F,stringsAsFactors = F)
+
+# create overall summary dataframe
+total_summary <- summarize(sus_coil, Mean=mean(PSI), Median=median(PSI), 
+                           Variance=var(PSI), SD=sd(PSI))
+
+# create summary by lot dataframe
+lot_summary <- sus_coil %>% group_by(Manufacturing_Lot) %>%
+  summarize(Mean=mean(PSI), Median=median(PSI), 
+            Variance=var(PSI), SD=sd(PSI), .groups = 'keep')
+
